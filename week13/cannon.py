df039bc5009d6724bc4f75a2b5c9389cf74ed3f2
jescubido <jarisse.escubido@gmail.com>
2023-05-16 23:11:57 -0700

    create rectangle projectile

--- week13/cannon.py
+++ week13/cannon.py
@@ -79,6 +79,68 @@ class Shell(GameObject):
         '''
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
+class RectangleProjectile(GameObject):
+    '''
+    The ball class. Creates a ball, controls it's movement and implement it's rendering.
+    '''
+
+    def __init__(self, coord, vel, height=20, width=30, color=None):
+        '''
+        Constructor method. Initializes ball's parameters and initial values.
+        '''
+        self.coord = coord
+        self.vel = vel
+        if color == None:
+            color = rand_color()
+        self.color = color
+        self.height = height
+        self.width = width
+        self.is_alive = True
+
+    def check_corners(self, refl_ort=0.8, refl_par=0.9):
+        '''
+        Reflects ball's velocity when ball bumps into the screen corners. Implemetns inelastic rebounce.
+        '''
+        for i in range(2):
+            if self.coord[i] < self.height:
+                self.coord[i] = self.height
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1-i] = int(self.vel[1-i] * refl_par)
+            elif self.coord[i] > SCREEN_SIZE[i] - self.height:
+                self.coord[i] = SCREEN_SIZE[i] - self.height
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1-i] = int(self.vel[1-i] * refl_par)
+        for i in range(2):
+            if self.coord[i] < self.width:
+                self.coord[i] = self.width
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1-i] = int(self.vel[1-i] * refl_par)
+            elif self.coord[i] > SCREEN_SIZE[i] - self.width:
+                self.coord[i] = SCREEN_SIZE[i] - self.width
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1-i] = int(self.vel[1-i] * refl_par)
+
+    def move(self, time=1, grav=0):
+        '''
+        Moves the ball according to it's velocity and time step.
+        Changes the ball's velocity due to gravitational force.
+        '''
+        self.vel[1] += grav
+        for i in range(2):
+            self.coord[i] += time * self.vel[i]
+        self.check_corners()
+        if self.vel[0]**2 + self.vel[1]**2 < 2**2 and self.coord[1] > SCREEN_SIZE[1] - 2*self.height:
+            self.is_alive = False
+        self.check_corners()
+        if self.vel[0]**2 + self.vel[1]**2 < 2**2 and self.coord[1] > SCREEN_SIZE[1] - 2*self.width:
+            self.is_alive = False
+
+    def draw(self, screen):
+        '''
+        Draws the ball on appropriate surface.
+        '''
+        pg.draw.rect(screen, self.color, self.coord, self.height, self.width)
+
 
 class Cannon(GameObject):
     '''
@@ -117,6 +179,8 @@ class Cannon(GameObject):
         vel = self.pow
         angle = self.angle
         ball = Shell(list(self.coord), [
+                     int(vel * np.cos(angle)), int(vel * np.sin(angle))])
+        rectangle = RectangleProjectile(list(self.coord), [
                      int(vel * np.cos(angle)), int(vel * np.sin(angle))])
         self.pow = self.min_pow
         self.active = False
